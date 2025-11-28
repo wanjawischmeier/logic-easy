e41b5d2bae5e8393237b3742fb73f8aa2b003ed6
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2025-11-28 12:41:35 +0100

    Dockview working

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,80 @@
-import { createApp } from 'vue'
-import TestEins from './TestEins.vue'
-import router from './router'
-import type { DockviewReadyEvent } from 'dockview-vue';
+import 'dockview-vue/dist/styles/dockview.css';
+import { type PropType, createApp, defineComponent } from 'vue';
+import {
+  DockviewVue,
+  type DockviewReadyEvent,
+  type IDockviewPanelProps,
+} from 'dockview-vue';
 
-const App = {
+const Panel = defineComponent({
+  inject: ['vu3ProvideInjectEvidenceTestMessage'],
+  name: 'Panel',
+  props: {
+    params: {
+      type: Object as PropType<IDockviewPanelProps>,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      title: '',
+      message: this.vu3ProvideInjectEvidenceTestMessage ?? 'not found',
+    };
+  },
+  mounted() {
+    const disposable = this.params.api.onDidTitleChange(() => {
+      this.title = this.params.api.title;
+    });
+    this.title = this.params.api.title;
+
+    return () => {
+      disposable.dispose();
+    };
+  },
+  template: `
+    <div style="height:100%; color:red;">
+      Hello World
+      <div>{{title}}</div>
+      <div>{{message}}</div>
+    </div>`,
+});
+
+const App = defineComponent({
   name: 'App',
   components: {
-    'component_1': TestEins,
-    'tab_1': TestEins,
+    'dockview-vue': DockviewVue,
+    panel: Panel,
   },
   methods: {
     onReady(event: DockviewReadyEvent) {
       event.api.addPanel({
         id: 'panel_1',
-        component: 'component_1',
-        tabComponent: 'tab_1'
+        component: 'panel',
+        title: 'Panel 1',
+      });
+      event.api.addPanel({
+        id: 'panel_2',
+        component: 'panel',
+        title: 'Panel 2',
       });
     },
   },
+  provide() {
+    return {
+      vu3ProvideInjectEvidenceTestMessage: 'Hello from the provider',
+    };
+  },
   template: `
       <dockview-vue
+        style="width:100vw; height:100vh"
+        class="dockview-theme-abyss"
         @ready="onReady"
       >
       </dockview-vue>`,
+});
+
+const app = createApp(App);
+app.config.errorHandler = (err) => {
+  console.log(err);
 };
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.mount(document.getElementById('app')!);
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      // simple empty component - the app root contains the dockview
+      component: {
+        template: '<div></div>'
+      }
+    },
+  ],
 })
 
 export default router
