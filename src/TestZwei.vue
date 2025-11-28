e6e53154b8b669fc3e22a45a7913121aee93cc98
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2025-11-28 11:53:31 +0100

    Add dockview

---
+++ src/TestZwei.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Test2</h1>
+</template>
+
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import TestEins from './TestEins.vue'
 import router from './router'
+import type { DockviewReadyEvent } from 'dockview-vue';
+
+const App = {
+  name: 'App',
+  components: {
+    'component_1': TestEins,
+    'tab_1': TestEins,
+  },
+  methods: {
+    onReady(event: DockviewReadyEvent) {
+      event.api.addPanel({
+        id: 'panel_1',
+        component: 'component_1',
+        tabComponent: 'tab_1'
+      });
+    },
+  },
+  template: `
+      <dockview-vue
+        @ready="onReady"
+      >
+      </dockview-vue>`,
+};
 
 const app = createApp(App)
 
