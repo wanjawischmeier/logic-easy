54c4fce7144cae9691f69511fe0fe8c8dad62643
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2025-12-06 23:34:09 +0100

    Add close button to header

--- src/components/DockViewHeader.vue
+++ src/components/DockViewHeader.vue
@@ -14,21 +14,28 @@
         <HeaderMenuBar />
       </div>
 
-      <div v-if="currentProjectInfo" class="w-50 flex items-center shrink-0 max-w-full bg-surface-2 rounded-xs
+      <div v-if="currentProjectInfo" class="flex">
+        <div class="w-45 flex items-center shrink-0 max-w-full bg-surface-2 rounded-l-xs
   border border-transparent
   hover:border-gray-300
   focus-within:border-primary
   focus-within:hover:border-primary">
-        <!-- TODO: Move into svg file -->
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"
-          class="fill-primary-variant">
-          <path
-            d="M 6 2 C 4.9057453 2 4 2.9057453 4 4 L 4 20 C 4 21.094255 4.9057453 22 6 22 L 18 22 C 19.094255 22 20 21.094255 20 20 L 20 8 L 14 2 L 6 2 z M 6 4 L 13 4 L 13 9 L 18 9 L 18 20 L 6 20 L 6 4 z" />
-        </svg>
+          <!-- TODO: Move into svg file -->
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"
+            class="fill-primary-variant">
+            <path
+              d="M 6 2 C 4.9057453 2 4 2.9057453 4 4 L 4 20 C 4 21.094255 4.9057453 22 6 22 L 18 22 C 19.094255 22 20 21.094255 20 20 L 20 8 L 14 2 L 6 2 z M 6 4 L 13 4 L 13 9 L 18 9 L 18 20 L 6 20 L 6 4 z" />
+          </svg>
 
-        <input ref="projectInput" type="text" placeholder="Project Name" maxlength="40" v-model="projectValue"
-          class="flex-1 bg-transparent outline-none min-w-0 p-0.5 truncate text-primary-variant focus-within:text-on-surface"
-          @keydown.enter="handleProjectEnter" />
+          <input ref="projectInput" type="text" placeholder="Project Name" maxlength="40" v-model="projectValue"
+            class="flex-1 bg-transparent outline-none min-w-0 p-0.5 truncate text-primary-variant focus-within:text-on-surface"
+            @keydown.enter="handleProjectEnter" />
+        </div>
+
+        <button type="button" @click="stateManager.closeCurrentProject" aria-label="Close project" title="Close project"
+          class="h-full px-3 text-xl bg-surface-2 rounded-l-none rounded-r-xs hover:bg-primary-variant">
+          <span aria-hidden="true">×</span>
+        </button>
       </div>
     </div>
   </header>
@@ -38,6 +45,7 @@
 import { ref, watch, computed } from 'vue';
 import HeaderMenuBar from './HeaderMenuBar.vue';
 import { projectManager } from '@/utility/states/projectManager';
+import { stateManager } from '@/utility/states/stateManager';
 
 const projectInput = ref<HTMLInputElement>()
 const currentProjectInfo = computed(() => projectManager.getCurrentProjectInfo())
