160fbd3f16fc19e8c90c3138f7f551cde43a17bf
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2026-01-01 17:36:39 +0100

    Fix import

--- src/utility/truthtable/interpreter.ts
+++ src/utility/truthtable/interpreter.ts
@@ -1,5 +1,5 @@
-import type { TruthTableData } from "@/projects/Project";
 import { stateManager } from "@/projects/stateManager";
+import type { TruthTableData } from "@/projects/truth-table/TruthTableProject";
 import { minifyTruthTable } from "@/utility/truthtable/espresso";
 import { FunctionType, type Formula, type Literal, type Term } from "@/utility/types";
 
