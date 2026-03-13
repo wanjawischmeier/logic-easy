3639226e7d911366ba97c681cfa220eb13a8ca03
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2026-03-13 15:40:20 +0100

    Highlight base if submenu open

--- src/components/HeaderMenuBar.vue
+++ src/components/HeaderMenuBar.vue
@@ -225,16 +225,19 @@ const MenuList = defineComponent<MenuListProps>({
       h(
         'ul',
         { class: 'pr-1' },
-        props.items.map((entry, idx) =>
-          h(
+        props.items.map((entry, idx) => {
+          const submenuOpen = isOpen(level.value, idx)
+          return h(
             'li',
             { class: 'relative', key: idx },
             [
               h(
                 'button',
                 {
-                  class:
+                  class: [
                     'w-full text-left m-0.5 px-3 py-2 rounded-xs border-0! hover:bg-surface-3 disabled:bg-surface-2 disabled:text-on-surface-disabled flex justify-between text-sm',
+                    { 'bg-surface-3': submenuOpen }
+                  ],
                   disabled: (!entry.action && !entry.panelId && !entry.children) || entry.disabled,
                   onClick: entry.children ? undefined : () => runAction(entry),
                   onMouseenter: entry.children
@@ -248,7 +251,7 @@ const MenuList = defineComponent<MenuListProps>({
                   entry.children ? h('span', { class: 'opacity-70' }, '›') : null,
                 ],
               ),
-              entry.children && isOpen(level.value, idx)
+              submenuOpen
                 ? h(
                   'div',
                   {
@@ -259,8 +262,8 @@ const MenuList = defineComponent<MenuListProps>({
                 )
                 : null,
             ],
-          ),
-        ),
+          )
+        }),
       )
   },
 })
