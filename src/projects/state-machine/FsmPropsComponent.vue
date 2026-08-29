7af580f14b10cd12e94558679efc3432058f9530
aylinjosephine <aylinkutluk@web.de>
2026-08-29 16:52:06 +0200

    Include fsm engine code in formatting command

--- src/projects/state-machine/FsmPropsComponent.vue
+++ src/projects/state-machine/FsmPropsComponent.vue
@@ -46,8 +46,8 @@
       </div>
 
       <p class="mt-4 text-xs text-on-surface-disabled text-center">
-        The selected bit numbers determine the amount of input / output bits.
-        The bit count is fixed and cannot be changed after creation.
+        The selected bit numbers determine the amount of input / output bits. The bit count is fixed
+        and cannot be changed after creation.
       </p>
     </div>
   </div>
